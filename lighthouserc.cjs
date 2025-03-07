167ece8f5664cfd185e73860e9f19e94390bb931
morganb75 <morgan.bleunven@maif.fr>
2025-03-07 16:45:31 +0100

    test lighthouse multipages

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,22 +1,29 @@
 module.exports = {
-  ci: {
-    collect: {
-      numberOfRuns: 3,
-      url: ["http://localhost:4173"], // Assure-toi que le serveur tourne bien ici
-      settings: {
-        chromeFlags: "--no-sandbox --ignore-certificate-errors --disable-dev-shm-usage"
-      }
+    ci: {
+        collect: {
+            numberOfRuns: 3,
+            url: [
+                "http://localhost:4173",
+                "http://localhost:4173/login",
+                "http://localhost:4173/firstlogin",
+                "http://localhost:4173/user",
+                "http://localhost:4173/admin",
+                "http://localhost:4173/admin/preinscrire",
+            ], // Assure-toi que le serveur tourne bien ici
+            settings: {
+                chromeFlags: "--no-sandbox --ignore-certificate-errors --disable-dev-shm-usage"
+            }
+        },
+        assert: {
+            assertions: {
+                "categories:performance": ["warn", {minScore: 0.9}],
+                "categories:accessibility": ["warn", {minScore: 0.9}],
+                "categories:best-practices": ["warn", {minScore: 0.9}],
+                "categories:seo": ["warn", {minScore: 0.9}],
+            },
+        },
+        upload: {
+            target: "temporary-public-storage",
+        },
     },
-    assert: {
-      assertions: {
-        "categories:performance": ["warn", { minScore: 0.9 }],
-        "categories:accessibility": ["warn", { minScore: 0.9 }],
-        "categories:best-practices": ["warn", { minScore: 0.9 }],
-        "categories:seo": ["warn", { minScore: 0.9 }],
-      },
-    },
-    upload: {
-      target: "temporary-public-storage",
-    },
-  },
 };
