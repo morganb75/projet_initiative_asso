5fd6d12d31159910a7a8d0718c9ea6c7ac06ca28
morganb75 <morgan.bleunven@maif.fr>
2025-03-07 16:02:14 +0100

    test789

---
+++ .lighthouserc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 3,
+      url: ["http://localhost:3000"],
+      settings: {
+        startServerCommand: "npm run dev",
+      },
+    },
+    assert: {
+      assertions: {
+        "categories:performance": ["warn", { minScore: 0.9 }],
+        "categories:accessibility": ["warn", { minScore: 0.9 }],
+        "categories:best-practices": ["warn", { minScore: 0.9 }],
+        "categories:seo": ["warn", { minScore: 0.9 }],
+      },
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+  },
+};
